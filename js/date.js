0b20e825e6dd03fa86299216e77d7fd043583de9
agueroc <agu21022@byui.edu>
2022-01-09 00:03:48 -0300

    changes

--- js/date.js
+++ js/date.js
@@ -25,6 +25,6 @@ let d = new Date();
 let dayName = daynames[d.getDay()];
 let monthName = months[d.getMonth()];
 let year = d.getFullYear();
-let fulldate = dayName + "," + monthName + " " + d.getDate() +", " + d.getFullYear();
+let fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + d.getFullYear();
 
 document.getElementById("currentdate").textContent = fulldate;
